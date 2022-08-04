a83f87ff6e6f4d86bf52c61fa471597a1f5f425b
marcvilaribera <marcvilaribera@gmail.com>
2022-08-04 13:00:12 -0400

    Formatting dates

--- components/stats/WeeklyWeight.js
+++ components/stats/WeeklyWeight.js
@@ -1,3 +1,4 @@
+import moment from 'moment'
 import React, { useContext } from 'react'
 import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 import { WeightContext } from '../../context/WeightContext'
@@ -5,11 +6,22 @@ import { WeightContext } from '../../context/WeightContext'
 export default function WeeklyWeight (props) {
   const { weight } = useContext(WeightContext)
 
+  weight.sort(function (a, b) {
+    const keyA = new Date(a.date)
+    const keyB = new Date(b.date)
+    // Compare the 2 dates
+    if (keyA < keyB) return -1
+    if (keyA > keyB) return 1
+    return 0
+  })
+
+  const data = weight.map(entry => {
+    return { ...entry, date: moment(entry.date).format('DD/MM/YYYY') }
+  }).slice(0, 10)
+
   return (
     <ResponsiveContainer width="100%" height={300} id="chart">
-      <BarChart
-        data={weight}
-      >
+      <BarChart data={data} >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="date"/>
         <YAxis />
@@ -19,9 +31,7 @@ export default function WeeklyWeight (props) {
           <Bar dataKey="galones" fill="#82ca9d" />
           <Bar dataKey="plasticoduro" fill="#FFC898" />
           <Bar dataKey="basura" fill="#FF5C58" />
-
       </BarChart>
-
     </ResponsiveContainer>
   )
 }
