e9ea46c12a31d80cd24c821f0fa36c92ec2b2ad5
marcvilaribera <marcvilaribera@gmail.com>
2022-08-15 13:05:04 -0400

    Adding carboard config

--- configuration.js
+++ configuration.js
@@ -24,6 +24,21 @@ export const conf =
       { nombre: 'P4 Nuevo Este', color: '#FFC898', center: '18.420669,-70.824762' }],
     map_center: { lat: 18.416482964779416, lng: -70.82662731805073 }
   },
+  sabanayegua_cardboard: {
+    nombre: 'Sabana Yegua - Cartón',
+    file: 'cardboard_sy.json',
+    map_center: { lat: 18.4606607, lng: -70.8405734 },
+    barrios: [
+      { nombre: 'Barrio Blanco', color: '#FF95C5', center: '18.4562141,-70.829' },
+      { nombre: 'Barrio Pintado', color: '#8884d8', center: '18.4579091,-70.835' },
+      { nombre: 'Barrio Nuevo', color: '#82ca9d', center: '18.4600755,-70.8293113' },
+      { nombre: 'Ojo de Agua', color: '#FFC898', center: '18.4608084,-70.8335' },
+      { nombre: 'San Francisco', color: '#916BBF', center: '18.4640215,-70.8315343' },
+      { nombre: 'El Abanico', color: '#ffff94', center: '18.4629935,-70.8337693' },
+      { nombre: 'Los Cartones', color: '#C8C6C6', center: '18.4604671,-70.8372607' },
+      { nombre: 'Barrio Tranquilo', color: '#FF5C58', center: '18.4649761,-70.8411297' },
+      { nombre: 'Las Mercedes', color: '#5D5B5B', center: '18.4671061,-70.8334927' }]
+  },
   sample: {
     nombre: 'Sabana Yegua',
     file: 'sample.json',
