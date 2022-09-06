c97c8e28fc1f6ffb5b483db3eaf507d1c1268e3d
marcvilaribera <marcvilaribera@gmail.com>
2022-09-06 17:11:54 +0200

    Displaying date added in table

--- components/DataGridTable.js
+++ components/DataGridTable.js
@@ -4,13 +4,48 @@ import { Button, FormControlLabel, Radio, RadioGroup, Typography } from '@mui/ma
 import { DataGrid, esES, GridActionsCellItem, GridToolbar } from '@mui/x-data-grid'
 import moment from 'moment'
 import Link from 'next/link'
-import { useCallback, useContext, useState } from 'react'
+import { useCallback, useContext, useEffect, useState } from 'react'
 import { conf } from '../configuration'
 import { PdrContext } from '../context/PdrContext'
 import { TownContext } from '../context/TownContext'
 import { calculateAlert } from '../utils/pdr-management'
 import DeleteRowDialog from './DeleteRowDialog'
 import { GreenRadio, RedRadio, YellowRadio } from './RadioButtons'
+
+export function lastNweeks (recogida, n) {
+  const fiveMondays = [...Array(5).keys()].map(nWeek => moment().day('monday').subtract(nWeek, 'weeks'))
+
+  const lastNweeks = fiveMondays.map(monday => {
+    const r = recogida.find(week => week.date === monday.format('DD/MM/YYYY'))
+    if (r) {
+      return { date: r.date, wasCollected: r.wasCollected }
+    } else {
+      return { date: monday.format('DD/MM/YYYY'), wasCollected: 'null' }
+    }
+  })
+
+  const result = lastNweeks.map(date => {
+    let control
+    if (date.wasCollected === 'si') {
+      control = <GreenRadio checked={true} size='small' sx={{ p: 0 }}/>
+    } else if (date.wasCollected === 'no') {
+      control = <RedRadio checked={true} size='small' sx={{ p: 0 }}/>
+    } else if (date.wasCollected === 'cerrado') {
+      control = <Radio checked={true} color="default" size='small' sx={{ p: 0 }}/>
+    } else if (date.wasCollected === 'nada') {
+      control = <YellowRadio checked={true} size='small' sx={{ p: 0 }}/>
+    } else if (date.wasCollected === 'null') {
+      control = <Radio checked={false} color="default" size='small' sx={{ p: 0 }}/>
+    }
+
+    return <FormControlLabel control={control} label={<Typography variant="body2" color="textSecondary">{date.date}</Typography>} labelPlacement="top" key={date.date} />
+  }).reverse()
+
+  return (
+        <RadioGroup row>
+        { result }
+        </RadioGroup>)
+}
 
 export default function DataGridTable () {
   const { pdr, setPdr } = useContext(PdrContext)
@@ -19,6 +54,21 @@ export default function DataGridTable () {
   const barrios = []
   conf[town].barrios.forEach((barrio) => { barrios.push(barrio.nombre) })
   const categories = conf[town].categories
+
+  useEffect(() => {
+    fetch('http://localhost:5000/add-date-added', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(pdr)
+    }).then(function (response) {
+      return response.json()
+    }).then(function (json) {
+      setPdr(json)
+    })
+  }, [])
 
   const deleteRow = (id) => {
     const dataUpdate = pdr.filter((row) => row.internalId !== id)
@@ -114,6 +164,15 @@ export default function DataGridTable () {
       width: 200
     },
     {
+      field: 'dateAdded',
+      headerName: 'Añadido el día',
+      editable: true,
+      type: 'date',
+      width: 150,
+      valueGetter: (params) => { return moment(params.value, 'DD/MM/YYYY') },
+      valueFormatter: (params) => { return params.value.format('DD/MM/YYYY') }
+    },
+    {
       field: 'alerta',
       headerName: 'Alerta',
       editable: false,
@@ -121,46 +180,12 @@ export default function DataGridTable () {
       width: 100,
       valueGetter: calculateAlert
     },
-    { field: 'active', headerName: 'Activo', editable: false, type: 'boolean' },
     {
       field: 'recogida',
       headerName: 'Últimas 5 semanas',
       editable: false,
       renderCell: (params) => {
-        const recogida = params.value
-
-        const fiveMondays = [...Array(5).keys()].map(nWeek => moment().day('monday').subtract(nWeek, 'weeks'))
-
-        const lastNweeks = fiveMondays.map(monday => {
-          const r = recogida.find(week => week.date === monday.format('DD/MM/YYYY'))
-          if (r) {
-            return { date: r.date, wasCollected: r.wasCollected }
-          } else {
-            return { date: monday.format('DD/MM/YYYY'), wasCollected: 'null' }
-          }
-        })
-
-        const result = lastNweeks.map(date => {
-          let control
-          if (date.wasCollected === 'si') {
-            control = <GreenRadio checked={true} size='small' sx={{ p: 0 }}/>
-          } else if (date.wasCollected === 'no') {
-            control = <RedRadio checked={true} size='small' sx={{ p: 0 }}/>
-          } else if (date.wasCollected === 'cerrado') {
-            control = <Radio checked={true} color="default" size='small' sx={{ p: 0 }}/>
-          } else if (date.wasCollected === 'nada') {
-            control = <YellowRadio checked={true} size='small' sx={{ p: 0 }}/>
-          } else if (date.wasCollected === 'null') {
-            control = <Radio checked={false} color="default" size='small' sx={{ p: 0 }}/>
-          }
-
-          return <FormControlLabel control={control} label={<Typography variant="body2" color="textSecondary">{date.date}</Typography>} labelPlacement="top" key={date.date} />
-        }).reverse()
-
-        return (
-          <RadioGroup row>
-          { result }
-          </RadioGroup>)
+        return lastNweeks(params.value)
       },
       width: 600
     }
