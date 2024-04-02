769994180458ae4b8f34f91c4a3fcbf43330a27d
marcvilaribera <marcvilaribera@gmail.com>
2024-04-02 10:25:58 +0200

    Using firestore and fast api routers

--- fast_api/main.py
+++ fast_api/main.py
@@ -2,30 +2,41 @@ import json
 import os
 from typing import Any
 
+import firebase_admin
 import google.oauth2.credentials
 import uvicorn
+from fastapi import APIRouter, Body, FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from firebase_admin import credentials, firestore
 from google.auth.transport import requests
 from google.cloud import storage
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 from starlette.middleware.sessions import SessionMiddleware
-from time_series_data import get_time_series_data
 
-from fastapi import Body, FastAPI, HTTPException, Request
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
+cred = credentials.Certificate("fast_api/routers/firestore-service-account.json")
+firebase_app = firebase_admin.initialize_app(cred)
+
+from .routers import pdr, recogida
+from .time_series_data import get_time_series_data
 
 app = FastAPI()
 
+
+app.include_router(pdr.router)
+app.include_router(recogida.router)
+
+
 os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"] = "True"
 flow = Flow.from_client_secrets_file(
-    "./client_secret_.json",
+    "fast_api/client_secret_.json",
     scopes=[
         "https://www.googleapis.com/auth/userinfo.profile",
         "https://www.googleapis.com/auth/devstorage.full_control",
         "https://www.googleapis.com/auth/userinfo.email",
     ],
-    redirect_uri="https://reciclapp-dev-dot-norse-voice-343214.uc.r.appspot.com",
+    redirect_uri="http://localhost:3000",
 )
 
 origins = [
@@ -41,7 +52,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-with open("./client_secret_.json") as f:
+with open("fast_api/client_secret_.json") as f:
     data = json.load(f)
     client_id = data["web"]["client_id"]
 
@@ -77,33 +88,5 @@ def get_current_user(request: Request, id_token_param: str):
     profile = {"name": user["name"], "picture": user["picture"]}
     name = user["name"]
     picture = user["picture"]
-
+    print(name, picture)
     return profile
-
-
-@app.post("/time-series-data")
-def time_series_data(
-    start: str,
-    end: str,
-    barrio: str = "all",
-    categoria: str = "all",
-    payload: Any = Body(None),
-):
-    return get_time_series_data(payload, categoria, start, end, barrio)
-
-
-@app.get("/download-file")
-def download_file(request: Request, file: str, bucket: str, token: str):
-    # Load credentials
-    credentials = google.oauth2.credentials.Credentials(token)
-
-    storage_client = storage.Client("norse-voice", credentials=credentials)
-    bucket = storage_client.bucket(bucket)
-    blob = bucket.blob(file)
-    try:
-        contents = blob.download_as_bytes()
-    except Exception as e:
-        print(e.code)
-        raise HTTPException(status_code=403, detail="No permission")
-
-    return json.loads(contents)
---
+++ fast_api/routers/pdr.py
@@ -0,0 +1,104 @@
+import json
+from typing import Annotated, Union
+
+import firebase_admin
+from fastapi import APIRouter, Depends, HTTPException, Request
+from firebase_admin import credentials, firestore
+from google.auth.transport import requests
+from google.oauth2 import id_token
+from pydantic import BaseModel
+
+db = firestore.client()
+
+router = APIRouter()
+
+with open("fast_api/client_secret_.json") as f:
+    data = json.load(f)
+    client_id = data["web"]["client_id"]
+
+
+class User(BaseModel):
+    name: str
+    picture: str
+    email: str
+
+
+def get_current_user(id_token_param: str):
+    user = id_token.verify_oauth2_token(
+        str(id_token_param),
+        requests.Request(),
+        client_id,
+    )
+
+    profile = {"name": user["name"], "picture": user["picture"], "email": user["email"]}
+
+    return User(**profile)
+
+
+def valid_user(current_user: Annotated[User, Depends(get_current_user)]):
+    """Check if the user is a valid user"""
+    valid_users = [doc.to_dict()["email"] for doc in db.collection("users").stream()]
+
+    if current_user.email in valid_users:
+        return True
+    else:
+        raise HTTPException(
+            status_code=403, detail="You are not authorized to access this resource"
+        )
+
+
+class Pdr(BaseModel):
+    id: int
+    internal_id: int
+    nombre: str
+    descripcion: str
+    barrio: str
+    categoria: str
+    comunidad: str
+    date_added: str
+    lat: float
+    lng: float
+
+
+@router.get("/pdr/get_all", tags=["pdr"])
+async def get_pdrs(is_valid_user: Annotated[bool, Depends(valid_user)]):
+
+    collection = db.collection("pdr")
+    docs = collection.stream()
+    return [doc.to_dict() for doc in docs]
+
+
+@router.get("/pdr/get/{internal_id}", tags=["pdr"])
+async def get_pdr(
+    internal_id: str, is_valid_user: Annotated[bool, Depends(valid_user)]
+):
+    doc_ref = db.collection("pdr").document({internal_id})
+    doc = doc_ref.get()
+    return doc.to_dict()
+
+
+@router.post("/pdr/update/{internal_id}", tags=["pdr"])
+async def update_pdr(
+    internal_id: str, new_data: Pdr, is_valid_user: Annotated[bool, Depends(valid_user)]
+):
+    db.collection("pdr").document(f"{str(internal_id)}").update(new_data.dict())
+    return new_data
+
+
+@router.post("/pdr/add", tags=["pdr"])
+async def add_pdr(
+    new_pdr: Pdr, is_valid_user: Annotated[bool, Depends(valid_user)]
+) -> Pdr:
+    print(new_pdr)
+    print(new_pdr.internal_id)
+
+    db.collection("pdr").document(str(new_pdr.internal_id)).set(new_pdr.dict())
+    return new_pdr
+
+
+@router.delete("/pdr/delete/{internal_id}", tags=["pdr"])
+async def delete_pdr(
+    internal_id: str, is_valid_user: Annotated[bool, Depends(valid_user)]
+):
+    doc_ref = db.collection("pdr").document(f"{str(internal_id)}").delete()
+    return internal_id
