8cece131864e6aed3d956363f660f3c352a80627
marcvilaribera <marcvilaribera@gmail.com>
2024-04-02 10:30:18 +0200

    formatting changes

--- components/RadioButtonsGroup.js
+++ components/RadioButtonsGroup.js
@@ -3,61 +3,61 @@ import Radio from '@mui/material/Radio'
 import { useEffect, useState } from 'react'
 import { GreenRadio, RedRadio, YellowRadio } from './RadioButtons'
 
-export default function RadioButtonsGroup (props) {
+export default function RadioButtonsGroup(props) {
   const [selectedValue, setSelectedValue] = useState(props.initialValue)
   useEffect(() => setSelectedValue(props.initialValue), [props.initialValue])
 
   const handleChange = (event) => {
     setSelectedValue(event.target.value)
-    props.onChange(props.barrio, props.id, props.year, props.week, event.target.value)
+    props.onChange(props.internal_id, event.target.value)
   }
 
   return (
 
     <RadioGroup onChange={handleChange}>
       <FormControlLabel
+        value="si"
+        control={<GreenRadio
+          checked={selectedValue === 'si'}
           value="si"
-          control={<GreenRadio
-            checked={selectedValue === 'si'}
-            value="si"
-            name="pasar-puntos-radio-button-si"
-            inputProps={{ 'aria-label': 'si' }}
-          >
-          </GreenRadio>}
-          label="Sí"
-        />
-        <FormControlLabel
-          value="no"
-          control={<RedRadio
-            checked={selectedValue === 'no'}
-            value="no"
-            name="pasar-puntos-radio-button-no"
-            inputProps={{ 'aria-label': 'no' }}
-          />}
-          label="No"
-        />
+          name="pasar-puntos-radio-button-si"
+          inputProps={{ 'aria-label': 'si' }}
+        >
+        </GreenRadio>}
+        label="Sí"
+      />
       <FormControlLabel
+        value="no"
+        control={<RedRadio
+          checked={selectedValue === 'no'}
+          value="no"
+          name="pasar-puntos-radio-button-no"
+          inputProps={{ 'aria-label': 'no' }}
+        />}
+        label="No"
+      />
+      <FormControlLabel
+        value="cerrado"
+        control={<Radio
+          checked={selectedValue === 'cerrado'}
           value="cerrado"
-          control={<Radio
-            checked={selectedValue === 'cerrado'}
-            value="cerrado"
-            color="default"
-            name="pasar-puntos-radio-button-cerrado"
-            inputProps={{ 'aria-label': 'cerrado' }}
-          />}
-          label="Cerrado"
-        />
-        <FormControlLabel
+          color="default"
+          name="pasar-puntos-radio-button-cerrado"
+          inputProps={{ 'aria-label': 'cerrado' }}
+        />}
+        label="Cerrado"
+      />
+      <FormControlLabel
+        value="nada"
+        control={<YellowRadio
+          checked={selectedValue === 'nada'}
           value="nada"
-          control={<YellowRadio
-            checked={selectedValue === 'nada'}
-            value="nada"
-            name="pasar-puntos-radio-button-nada"
-            inputProps={{ 'aria-label': null }}
-          />}
-          label="No disponible"
-        />
+          name="pasar-puntos-radio-button-nada"
+          inputProps={{ 'aria-label': null }}
+        />}
+        label="No disponible"
+      />
 
-      </RadioGroup>
+    </RadioGroup>
   )
 }
