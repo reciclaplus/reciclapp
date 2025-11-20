cec32a196aaac3fbcbcb9d6d621fb84d3cbd2930
marcvilaribera <marcvilaribera@gmail.com>
2025-11-20 08:29:49 +0100

    Fix crossorigin attribute in preconnect link for Google Fonts

--- pages/_app.js
+++ pages/_app.js
@@ -64,7 +64,7 @@ function MyApp({ Component, pageProps }) {
                       <meta name="description" content="Listado de familias" />
                       <link rel="icon" type="image/png" href="/logo.png" />
                       <link rel="preconnect" href="https://fonts.googleapis.com" />
-                      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                      <link rel="preconnect" href="https://fonts.gstatic.com" />
                       <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet" />
                     </Head>
                     <Component {...pageProps} />
